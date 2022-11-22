9268684ffa371d87f8bf5b72010c2ff0330bc81d
relereal <mindfigment@protonmail.com>
2022-11-22 11:25:01 +0100

    first commit

---
+++ datastore.go
@@ -0,0 +1,94 @@
+package datastore
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Datastore struct {
+	db        *sql.DB
+	dbPath    string
+	tableName string
+}
+
+func NewDatastore(dbPath string, tableName string) *Datastore {
+	// Create database if not already present
+	if _, err := os.Stat(dbPath); err != nil {
+		file, err := os.Create(dbPath)
+		if err != nil {
+			panic(err)
+		}
+		file.Close()
+	}
+	return &Datastore{
+		dbPath:    dbPath,
+		tableName: tableName,
+	}
+}
+
+func (ds *Datastore) Connect() {
+	// Open connection to the database
+	db, err := sql.Open("sqlite3", ds.dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create table acting as key-value store (string: blob)
+	createTableQuery := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (key TEXT NOT NULL PRIMARY KEY, value BLOB)", ds.tableName)
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		panic(err)
+	}
+
+	createIndexQuery := fmt.Sprintf("CREATE INDEX IF NOT EXISTS key_index ON %s (key)", ds.tableName)
+	_, err = db.Exec(createIndexQuery)
+	if err != nil {
+		panic(err)
+	}
+
+	// Save db connection
+	ds.db = db
+}
+
+func (ds *Datastore) Has(ctx context.Context, key string) (bool, error) {
+	query := fmt.Sprintf("SELECT key FROM %s WHERE key = ?", ds.tableName)
+	keys, err := ds.db.Query(query, key)
+	if err != nil {
+		return false, err
+	}
+	defer keys.Close()
+
+	if keys.Next() {
+		return true, nil
+	}
+	return false, nil
+}
+
+func (ds *Datastore) Get(ctx context.Context, key string) ([]byte, error) {
+	query := fmt.Sprintf("SELECT value FROM %s WHERE key = ?", ds.tableName)
+	var value []byte
+	if err := ds.db.QueryRow(query, key).Scan(&value); err != nil {
+		return nil, err
+	}
+	return value, nil
+}
+
+func (ds *Datastore) Put(ctx context.Context, key string, content []byte) error {
+	has, err := ds.Has(ctx, key)
+	if err != nil {
+		return err
+	}
+	if has {
+		return nil
+	}
+	query := fmt.Sprintf("INSERT INTO %s (key, value) VALUES(?, ?)", ds.tableName)
+	_, err = ds.db.Exec(query, key, content)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ datastore_test.go
@@ -0,0 +1,60 @@
+package datastore
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"testing"
+)
+
+func getDs() *Datastore {
+	os.Mkdir("test", 0777)
+	os.Remove("test/testdb.db")
+	ds := NewDatastore("test/testdb.db", "keystore")
+	ds.Connect()
+	return ds
+}
+
+func clearDs(ds *Datastore) {
+	ds.db.Close()
+	os.RemoveAll("test")
+}
+
+func TestDs(t *testing.T) {
+	ds := getDs()
+	defer clearDs(ds)
+
+	key := "testkey"
+	value := []byte("testvalue")
+
+	has, err := ds.Has(context.Background(), key)
+	if err != nil {
+		t.Errorf("Expected no error but got %s", err)
+	}
+	if has {
+		t.Errorf("Expected has=false but got has=%t", has)
+	}
+
+	err = ds.Put(context.Background(), key, value)
+	if err != nil {
+		t.Errorf("Expected no error but got %s", err)
+	}
+
+	has, err = ds.Has(context.Background(), key)
+	if err != nil {
+		t.Errorf("Expected no error but got %s", err)
+	}
+	if !has {
+		t.Errorf("Expected has=true but got has=%t", has)
+	}
+
+	content, err := ds.Get(context.Background(), key)
+	if err != nil {
+		t.Errorf("Expected no error but got %s", err)
+	}
+
+	same := bytes.Compare(content, value)
+	if same != 0 {
+		t.Errorf("Expected same %s=%s", content, value)
+	}
+}
